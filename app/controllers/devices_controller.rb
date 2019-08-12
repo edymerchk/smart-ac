a481c507ecc565b9dce54f1af4dae3b506b6de36
Luis Edymerchk Laverde <edyhack@gmail.com>
2019-08-12 16:15:55 -0500

    Initial views for devices

---
+++ app/controllers/devices_controller.rb
@@ -0,0 +1,10 @@
+class DevicesController < BaseController
+
+  def index
+    @devices = Device.all
+  end
+
+  def show
+    @device = Device.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # Admin Site
   devise_for :admins
   root to: "home#index"
+  resources :devices, only: [:index, :show]
 
   # API
   namespace :api do
