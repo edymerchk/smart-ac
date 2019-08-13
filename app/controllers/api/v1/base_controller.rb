24dd3f1ec9858209fd7c42d5268fb8154f9d85f1
Luis Edymerchk Laverde <edyhack@gmail.com>
2019-08-13 16:51:44 -0500

    Disable CSRF for API

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class BaseController < ApplicationController
+      protect_from_forgery with: :null_session
+
       include AuthenticationFilter
       include JsonApiErrors
     end
