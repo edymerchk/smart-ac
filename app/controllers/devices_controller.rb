408bc58130e1db7b180611c78ec760597d12b17a
Luis Edymerchk Laverde <edyhack@gmail.com>
2019-08-13 10:28:24 -0500

    View for device with latest report data

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -2,10 +2,12 @@ class DevicesController < BaseController
 
   def index
     @devices = Device.all
-    @devices = @devices.search(params[:search][:serial]) if params[:search][:serial].present?
+    @devices = @devices.search(params[:search][:serial]) if params.dig(:search, :serial).present?
   end
 
   def show
     @device = Device.find(params[:id])
+
+    @latest_report = @device.reports.last
   end
 end
